a7df1e23b1ae386190971f257a472ca9944fcc32
User <User@DESKTOP-A0GB0BF>
2023-09-15 23:02:04 +0100

    Add project files.

---
+++ balaMyFirstCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balaMyFirstCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World_This is Bala Writing first code");
+            
+            int number = 42;
+            Console.WriteLine(number);
+
+            float floatvalue = 3.63746477f;
+            Console.WriteLine(floatvalue);
+
+            decimal decNum = 3.1472538m;
+            Console.WriteLine(decNum);
+
+            DateTime CurrDateTime = DateTime.Now;
+            Console.WriteLine(CurrDateTime);
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
